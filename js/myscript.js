e2987ef5eada29ba223ad9d464dde25b3b2287c4
Himesh Ravi <ravi.himesh@gmail.com>
2019-05-05 17:30:30 +1000

    resonate test

---
+++ js/myscript.js
@@ -0,0 +1,66 @@
+// TODO: Modify this function
+function generateShortCode(storeId, transactionId) {
+    // Logic goes here
+    return document.getElementById("code").value;
+}
+
+// TODO: Modify this function
+function decodeShortCode(shortCode) {
+    // Logic goes here
+
+    return {
+        storeId: 0, // store id goes here,
+        shopDate: new Date(), // the date the customer shopped,
+        transactionId: 0, // transaction id goes here
+    };
+}
+
+// ------------------------------------------------------------------------------//
+// --------------- Don't touch this area, all tests have to pass --------------- //
+// ------------------------------------------------------------------------------//
+function RunTests() {
+
+    var storeIds = [175, 42, 0, 9]
+    var transactionIds = [9675, 23, 123, 7]
+
+    storeIds.forEach(function (storeId) {
+        transactionIds.forEach(function (transactionId) {
+            var shortCode = generateShortCode(storeId, transactionId);
+            var decodeResult = decodeShortCode(shortCode);
+            $("#test-results").append("<div>" + storeId + " - " + transactionId + ": " + shortCode + "</div>");
+            AddTestResult("Length <= 9", shortCode.length <= 9);
+            AddTestResult("Is String", (typeof shortCode === 'string'));
+            AddTestResult("Is Today", IsToday(decodeResult.shopDate));
+            AddTestResult("StoreId", storeId === decodeResult.storeId);
+            AddTestResult("TransId", transactionId === decodeResult.transactionId);
+        })
+    })
+}
+
+function IsToday(inputDate) {
+    // Get today's date
+    var todaysDate = new Date();
+    // call setHours to take the time out of the comparison
+    return (inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0));
+}
+
+function AddTestResult(testName, testResult) {
+    var div = $("#test-results").append("<div class='" + (testResult ? "pass" : "fail") + "'><span class='tname'>- " + testName + "</span><span class='tresult'>" + testResult + "</span></div>");
+}
+
+// ===== Scroll to Top ====
+$(document).ready(function () {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 250) {        // If page is scrolled more than 250px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);                                // Scroll delay time
+        return false;
+    });
+});
