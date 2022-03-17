9c449ba0d237d7534bfa0541d02d470c9fea9d6f
Chiran <62375799+kchiran@users.noreply.github.com>
2022-03-17 14:07:36 +1100

    Update myscript.js
    fix nine characters issue

--- js/myscript.js
+++ js/myscript.js
@@ -8,10 +8,11 @@ function random_transactionId(transactionIds) {
 var storeIds = [175, 42, 0, 9];
 var transactionIds = [9675, 23, 123, 7];
 
+
 // TODO: Modify this function
 function generateShortCode(storeId, transactionId) {
-  // Logic goes here
-  var date = new Date().getTime();
+    // Logic goes here
+    var date = new Date().getTime();
   var uuid = 'x'.replace(/[xy]/g, function(c) {
     var appendedID = "" + storeId.toString().length + transactionId.toString().length + storeId + transactionId;
     var sar = ["0", "00", "000", "0000", "00000", "000000", "0000000"];
@@ -25,16 +26,24 @@ function generateShortCode(storeId, transactionId) {
       var randomnumber = Math.floor(Math.random() * ((9 - appendedID.length) - das[l + 1] + 1)) + das[l + 1];
       docketNumber = "" + appendedID + (9 - appendedID.length) + randomnumber;
     }
-    //return docketNumber;
+    // return docketNumber;
     return (c == 'x' ? docketNumber : 0000000);
   });
-  return uuid;
+  // return uuid;
+  if (uuid.length >9){
+    return uuid.slice(0, -1);
+  }
+  else {
+    return uuid;
+  }
+  //str.slice(0, -1);
 }
 
 // TODO: Modify this function
 function decodeShortCode(shortCode) {
-  // Logic goes here
-  var sLen = shortCode.substring(0, 1);
+    // Logic goes here
+
+    var sLen = shortCode.substring(0, 1);
   var tLen = shortCode.substring(1, 2);
   var iLen = sLen + tLen
   return {
@@ -42,7 +51,6 @@ function decodeShortCode(shortCode) {
     shopDate: new Date(), // the date the customer shopped,
     transactionId: parseInt(shortCode.substring(parseInt(sLen, 10) + iLen.length, iLen.length + parseInt(sLen, 10) + parseInt(tLen, 10)), 10) // transaction id goes here
   };
-
 }
 
 // ------------------------------------------------------------------------------//
@@ -50,52 +58,30 @@ function decodeShortCode(shortCode) {
 // ------------------------------------------------------------------------------//
 function RunTests() {
 
-  var storeIds = [175, 42, 0, 9]
-  var transactionIds = [9675, 23, 123, 7]
+    var storeIds = [175, 42, 0, 9]
+    var transactionIds = [9675, 23, 123, 7]
 
-  storeIds.forEach(function(storeId) {
-    transactionIds.forEach(function(transactionId) {
-      var shortCode = generateShortCode(storeId, transactionId);
-      var decodeResult = decodeShortCode(shortCode);
-      $("#test-results").append("<div>" + storeId + " - " + transactionId + ": " + shortCode + "</div>");
-      AddTestResult("Length <= 9", shortCode.length <= 9);
-      AddTestResult("Is String", (typeof shortCode === 'string'));
-      AddTestResult("Is Today", IsToday(decodeResult.shopDate));
-      AddTestResult("StoreId", storeId === decodeResult.storeId);
-      AddTestResult("TransId", transactionId === decodeResult.transactionId);
+    storeIds.forEach(function (storeId) {
+        transactionIds.forEach(function (transactionId) {
+            var shortCode = generateShortCode(storeId, transactionId);
+            var decodeResult = decodeShortCode(shortCode);
+            $("#test-results").append("<div>" + storeId + " - " + transactionId + ": " + shortCode + "</div>");
+            AddTestResult("Length <= 9", shortCode.length <= 9);
+            AddTestResult("Is String", (typeof shortCode === 'string'));
+            AddTestResult("Is Today", IsToday(decodeResult.shopDate));
+            AddTestResult("StoreId", storeId === decodeResult.storeId);
+            AddTestResult("TransId", transactionId === decodeResult.transactionId);
+        })
     })
-  })
 }
 
 function IsToday(inputDate) {
-  // Get today's date
-  var todaysDate = new Date();
-  // call setHours to take the time out of the comparison
-  return (inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0));
+    // Get today's date
+    var todaysDate = new Date();
+    // call setHours to take the time out of the comparison
+    return (inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0));
 }
 
 function AddTestResult(testName, testResult) {
-  var div = $("#test-results").append("<div class='" + (testResult ? "pass" : "fail") + "'><span class='tname'>- " + testName + "</span><span class='tresult'>" + testResult + "</span></div>");
+    var div = $("#test-results").append("<div class='" + (testResult ? "pass" : "fail") + "'><span class='tname'>- " + testName + "</span><span class='tresult'>" + testResult + "</span></div>");
 }
-
-// funtion to clear the results Div
-function clearDIV() {
-  document.getElementById("test-results").innerHTML = "";
-}
-
-// ===== Scroll to Top ====
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 250) {
-      $('#return-to-top').fadeIn(200);
-    } else {
-      $('#return-to-top').fadeOut(200);
-    }
-  });
-  $('#return-to-top').click(function() {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 500);
-    return false;
-  });
-});
